ce87f8387eac28b6573414cb83062a36cf024049
joaovq <vitorsnta90@gmail.com>
2023-01-09 19:07:53 -0300

    add annotation Repository

--- src/main/java/br/com/cadastroApi/repository/EnderecoRepository.java
+++ src/main/java/br/com/cadastroApi/repository/EnderecoRepository.java
@@ -2,6 +2,8 @@ package br.com.cadastroApi.repository;
 
 import br.com.cadastroApi.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, String> {
 }
--- src/main/java/br/com/cadastroApi/repository/PessoaRepository.java
+++ src/main/java/br/com/cadastroApi/repository/PessoaRepository.java
@@ -1,8 +1,11 @@
 package br.com.cadastroApi.repository;
 
 import br.com.cadastroApi.entities.Pessoa;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
 }
