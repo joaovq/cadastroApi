677ff25d8beae00a23510f355daa7b8f693690e5
joaovq <vitorsnta90@gmail.com>
2023-01-10 00:22:51 -0300

    create PessoaServiceImplTest.java

---
+++ src/test/java/br/com/cadastroApi/service/impl/PessoaServiceImplTest.java
@@ -0,0 +1,33 @@
+package br.com.cadastroApi.service.impl;
+
+import br.com.cadastroApi.entities.Pessoa;
+import br.com.cadastroApi.repository.PessoaRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getPessoaById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+}
