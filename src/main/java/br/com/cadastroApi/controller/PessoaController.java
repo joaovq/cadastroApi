2e2290a4179f43fbe063a5090ce5c5b92e3a7cac
joaovq <vitorsnta90@gmail.com>
2023-01-10 15:02:15 -0300

    complete method update

--- src/main/java/br/com/cadastroApi/controller/PessoaController.java
+++ src/main/java/br/com/cadastroApi/controller/PessoaController.java
@@ -1,11 +1,14 @@
 package br.com.cadastroApi.controller;
 
 import br.com.cadastroApi.entities.Pessoa;
+import br.com.cadastroApi.entities.forms.PessoaForm;
 import br.com.cadastroApi.service.impl.PessoaServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -20,27 +23,28 @@ public class PessoaController {
     * */
     @Autowired
     private PessoaServiceImpl service;
-
+//    Verificar todas as pessoas
     @GetMapping
     public ResponseEntity<List<Pessoa>> getAll(){
         List<Pessoa> pessoas = service.getAll();
         return ResponseEntity.ok().body(pessoas);
     }
-
+//      Encontrar pessoa pelo id
     @GetMapping("/{id}")
     public ResponseEntity<Pessoa> getById(@PathVariable Long id){
         Pessoa pessoa = service.getPessoaById(id);
         return ResponseEntity.ok().body(pessoa);
     }
-
+    //  Criar pessoa
     @PostMapping
-    public ResponseEntity<Pessoa> create(@RequestBody Pessoa pessoa){
-        service.insert(pessoa);
+    public ResponseEntity<Pessoa> create(@Valid @RequestBody PessoaForm pessoaForm) throws ParseException {
+        Pessoa pessoa = service.insert(pessoaForm);
         return ResponseEntity.status(201).body(pessoa);
     }
+//    Atualizar pessoa
     @PutMapping("/put/{id}")
-    public ResponseEntity<String> update(@PathVariable Long id,@RequestBody Pessoa pessoa){
-        service.update(id, pessoa);
-        return ResponseEntity.status(201).body("update complete");
+    public ResponseEntity<Pessoa> update(@PathVariable Long id,@RequestBody PessoaForm pessoaForm) throws ParseException {
+        Pessoa pessoaUpdate = service.update(id, pessoaForm);
+        return ResponseEntity.status(201).body(pessoaUpdate);
     }
 }
