82f3dbe937e0823e8ec494a9414f41114b02e721
joaovq <vitorsnta90@gmail.com>
2023-01-10 15:04:25 -0300

    add method getAdressPrincipal

--- src/main/java/br/com/cadastroApi/service/EnderecoService.java
+++ src/main/java/br/com/cadastroApi/service/EnderecoService.java
@@ -1,10 +1,13 @@
 package br.com.cadastroApi.service;
 
 import br.com.cadastroApi.entities.Endereco;
+import br.com.cadastroApi.entities.forms.EnderecoForm;
 
 import java.util.List;
 
 public interface EnderecoService {
-    Endereco insert(Endereco endereco);
+    Endereco insert(EnderecoForm endereco);
     List<Endereco> getAll();
+    List<Endereco> getById(Long id);
+    Endereco getAdressPrincipal(Long id);
 }
