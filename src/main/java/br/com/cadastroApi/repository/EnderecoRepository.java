fb8a677970ce762c686fc3e34b8a9ab03dab5e19
joaovq <vitorsnta90@gmail.com>
2023-01-10 15:07:40 -0300

    create method query encontrarEnderecosPessoa and encontrarEnderecoPricipalDaPessoa

--- src/main/java/br/com/cadastroApi/repository/EnderecoRepository.java
+++ src/main/java/br/com/cadastroApi/repository/EnderecoRepository.java
@@ -2,8 +2,16 @@ package br.com.cadastroApi.repository;
 
 import br.com.cadastroApi.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    @Query("SELECT e FROM Endereco e WHERE e.pessoa.id = :id")
+    List<Endereco> encontrarEnderecosDaPessoa(Long id);
+    @Query("SELECT e FROM Endereco e WHERE e.pessoa.id = :id and e.ePrincipal = true")
+    Endereco encontrarEnderecoPricipalDaPessoa(Long id);
 }
