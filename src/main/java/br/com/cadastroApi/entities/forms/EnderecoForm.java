b61dcd25c22cd8a01aaeecbff05a796f7e33daa9
joaovq <vitorsnta90@gmail.com>
2023-01-10 15:49:58 -0300

    Forms

--- src/main/java/br/com/cadastroApi/entities/forms/EnderecoForm.java
+++ src/main/java/br/com/cadastroApi/entities/forms/EnderecoForm.java
@@ -1,6 +1,7 @@
 package br.com.cadastroApi.entities.forms;
 
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 public class EnderecoForm {
     @Positive(message = "o id tem que ser positivo")
     private Long pessoaId;
+    @Size(max = 8, min = 8)
     private String cep;
+    @Size(max = 3)
     private String numero;
     private String complemento;
 }
--- src/main/java/br/com/cadastroApi/entities/forms/PessoaForm.java
+++ src/main/java/br/com/cadastroApi/entities/forms/PessoaForm.java
@@ -1,5 +1,6 @@
 package br.com.cadastroApi.entities.forms;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
