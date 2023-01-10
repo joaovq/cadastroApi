0272b34c1f419da63bf0efbf0054d2a216b27e69
joaovq <vitorsnta90@gmail.com>
2023-01-10 15:01:46 -0300

    modify property endereco, nome e dataDeNascimento

--- src/main/java/br/com/cadastroApi/entities/Pessoa.java
+++ src/main/java/br/com/cadastroApi/entities/Pessoa.java
@@ -1,18 +1,19 @@
 package br.com.cadastroApi.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "pessoa_tb")
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Pessoa {
 //    TODO: adicionar lista de endereços na propertie endereco
 //      Avaliar a adição de PessoaEndereco
@@ -20,12 +21,12 @@ public class Pessoa {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NonNull
-    private String name;
+    private String nome;
     @NonNull
-    private String birth;
-    @NonNull
-    @ManyToOne
-    private Endereco endereco;
+    private String dataDeNascimento;
+    @OneToMany(mappedBy = "pessoa", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Endereco> endereco;
 
 
 }
