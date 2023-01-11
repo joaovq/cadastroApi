460ca8d90f325412cef202977ca32980a2b0a963
joaovq <vitorsnta90@gmail.com>
2023-01-11 14:04:13 -0300

    implements LocalDate property on create method

--- src/main/java/br/com/cadastroApi/controller/PessoaController.java
+++ src/main/java/br/com/cadastroApi/controller/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
